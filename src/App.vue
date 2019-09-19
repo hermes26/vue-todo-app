17c8c5fe1e9a99b886337a3e54f76016c89b6425
hermes26 <hermitte.thierry@gmail.com>
2019-09-19 18:03:18 +0200

    checkbox with event added to todo items

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    {{todos}}
     <Todos v-bind:lists="todos"/>
   </div>
 </template>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="todo-item" v-bind:class="{'is-complete':todo.completed}"> <!-- we are binding a class on the condition that todo.completed is true -->
+        <input type="checkbox" v-on:change="markComplete">
+        <!-- v-on directives is for events eg v-on:click -->
+        <!-- we are looking at a change event and the ncalling markComplete -->
         <p>{{todo.title}}</p>
     </div>
 </template>
@@ -7,7 +10,12 @@
 <script>
 export default {
     name: "TodoItem",
-    props: ["todo"]
+    props: ["todo"],
+    methods: {
+        markComplete(){
+            this.todo.completed = !this.todo.completed;
+        }
+    }
 }
 </script>
 

f59ea6cb982ba0ab35886be6eecea8025be22e9e
hermes26 <hermitte.thierry@gmail.com>
2019-09-19 16:15:58 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue'  //importing vue
+import App from './App.vue' //importing the main app component
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue({ //creating a new vue instance
+  render: h => h(App), //rendering the main app component, inside of..
+}).$mount('#app') //a div or any other element with the id of app(#app)

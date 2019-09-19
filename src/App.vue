8d34df5130cf8177ddbf5be385468a439d5445de
hermes26 <hermitte.thierry@gmail.com>
2019-09-19 17:13:02 +0200

    todos component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,42 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{todos}}
+    <Todos v-bind:lists="todos"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Todos,
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo Two',
+          completed: true
+        },
+        {
+          id: 1,
+          title: 'Todo Three',
+          completed: false
+        },
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-bind:key="list.id" v-for="list in lists">
+            <h3>{{list.title}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Todos',
+    props: ["lists"]
+}
+</script>
+
+<style scoped>
+
+</style>
